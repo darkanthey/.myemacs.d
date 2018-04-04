a3559e9b7dfedb84b18a4fb9425a4d0712b3ad42
Andrew Grytsenko <Andrew.Grytsenko@creative-assembly.com>
2018-04-04 15:11:29 +0100

    Rust Dev 1.27 released. Change docker builder for support rust-toolchain for docker and remacs libs.

--- builder.sh
+++ builder.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-docker build -f docker/ubuntu/Dockerfile -t remacs-build .
-docker run --rm --security-opt seccomp=unconfined -v ~/Documents/libs/remacs:/home/darkanthey/remacs_src -v ~/.local:/home/darkanthey/.local -it remacs-build
+REMACS_DIR=~/Documents/libs/remacs
 
+./docker/generate
+
+cp -v ./docker/rust-toolchain $REMACS_DIR
+
+docker build -f docker/ubuntu/Dockerfile -t remacs-build .
+docker run --rm --security-opt seccomp=unconfined -v $REMACS_DIR:/home/darkanthey/remacs_src -v ~/.local:/home/darkanthey/.local -it remacs-build
 
 ### Add PATH
 [[ ":$PATH:" != *":~/.local/bin:"* ]] && PATH="~/.local/bin:${PATH}"
